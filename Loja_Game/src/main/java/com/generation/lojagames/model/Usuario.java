207cbe3fab9e4733c750de8780f90f150a611e81
edenilson <edenilson.dy@gmail.com>
2022-05-12 18:03:16 -0300

    adds security

---
+++ Loja_Game/src/main/java/com/generation/lojagames/model/Usuario.java
@@ -0,0 +1,74 @@
+package com.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo Nome precisa ser preenchido")
+	private String nome;
+	
+	@NotNull(message = "O atrubuto Usuario deve ser preenchido")
+	@Email(message = "O atributo Email devet ter um email válido!!")
+	private String usuario;
+	
+	@NotBlank(message = "O atibuto Senha é obrigatótrio!!")
+	@Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres!!")
+	private String senha;
+	
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
--- Loja_Game/src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
+++ Loja_Game/src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
@@ -3,6 +3,7 @@ package com.generation.lojagames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.lojagames.model.Categoria;
@@ -10,6 +11,6 @@ import com.generation.lojagames.model.Categoria;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	
-	public List<Categoria> findAllByTipoContainingIgnoreCase(String Tipo);
+	public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo")String Tipo);
 
 }
